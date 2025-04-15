4784e7ee90dade6eeb04e815e2e443cb0f9eba7b
shreyas <thmshreyas@gmail.com>
2025-04-16 00:40:50 +0530

    progress 2

---
+++ auth/authServer.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const app = express();
+const jwt = require('jsonwebtoken');
+require('dotenv').config()
+app.use(express.json());
+
+const posts=[
+    {
+        username:'john',
+        title:'Post 1'
+    },
+    {
+        username:'jane',    
+        title:'Post 2'
+    }
+]
+
+app.get('/posts',authenticateToken,(req,res)=>{
+    res.json(posts.filter(post=>post.username===req.user.name))
+})
+let refreshTokens=[];
+app.post('/token',(req,res)=>{
+
+  const refreshToken=req.body.token;
+  if(refreshToken==null) return res.sendStatus(401);
+  if(!refreshTokens.includes(refreshToken)) return res.sendStatus(403);
+  jwt.verify(refreshToken,process.env.REFRESH_TOKEN_SECRET,(err,user)=>{
+      if(err) return res.sendStatus(403);
+      const accessToken=generateAccessToken({name:user.name})
+      ; 
+  })
+})
+app.post('/login',(req,res)=>{
+    const username=req.body.username;
+    const user={name:username};
+    const accessToken=generateAccessToken(user);
+    refreshTokens.push(generateRefreshToken(user));
+    const refreshToken=jwt.sign(user,process.env.REFRESH_TOKEN_SECRET);
+    res.json({accessToken:accessToken,refreshToken:refreshToken});
+})
+function authenticateToken(req,res,next){
+    const authHeader=req.headers['authorization'];
+    const token=authHeader && authHeader.split(' ')[1];
+    if(token==null) return res.sendStatus(401);
+    jwt.verify(token,process.env.ACCESS_TOKEN_SECRET,(err,user)=>{
+        if(err) return res.sendStatus(403);
+        req.user=user;
+        next();
+    })
+ 
+}
+function generateAccessToken(user){
+    return jwt.sign(user,process.env.ACCESS_TOKEN_SECRET,{expiresIn:'15s'}) 
+}
+
+app.listen(4000, () => {
+  console.log('Server is running on http://localhost:4000');
+})
