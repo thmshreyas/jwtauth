ef599baf7c961774c36e695c7ce1e7670fd19473
shreyas <thmshreyas@gmail.com>
2025-04-16 00:16:51 +0530

    progress

--- auth/server.js
+++ auth/server.js
@@ -14,15 +14,28 @@ const posts=[
         title:'Post 2'
     }
 ]
-app.get('/posts',(req,res)=>{
-    res.json(posts)
+
+app.get('/posts',authenticateToken,(req,res)=>{
+    res.json(posts.filter(post=>post.username===req.user.name))
 })
 app.post('/login',(req,res)=>{
     const username=req.body.username;
     const user={name:username};
-    jwt.sign(user,process.env.ACCESS_TOKEN_SECRET)
+    const accessToken=jwt.sign(user,process.env.ACCESS_TOKEN_SECRET)
     res.json({accessToken:accessToken})
 })
+function authenticateToken(req,res,next){
+    const authHeader=req.headers['authorization'];
+    const token=authHeader && authHeader.split(' ')[1];
+    if(token==null) return res.sendStatus(401);
+    jwt.verify(token,process.env.ACCESS_TOKEN_SECRET,(err,user)=>{
+        if(err) return res.sendStatus(403);
+        req.user=user;
+        next();
+    })
+ 
+}
+
 app.listen(3000, () => {
   console.log('Server is running on http://localhost:3000');
 })
